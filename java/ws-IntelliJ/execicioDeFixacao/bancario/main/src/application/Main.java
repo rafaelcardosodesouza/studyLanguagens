576bfdf38e2ab99eae31914082344d879d6d144d
rafael <rafaelcsadm@hotmail.com>
2023-10-28 15:36:33 -0300

    update

--- java/ws-IntelliJ/execicioDeFixacao/bancario/main/src/application/Main.java
+++ java/ws-IntelliJ/execicioDeFixacao/bancario/main/src/application/Main.java
@@ -1,60 +1,64 @@
 package application;
-import entites.User;
+import entites.Account;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User();
+        Locale.setDefault(Locale.US);
+        Account account;
         Scanner sc = new Scanner(System.in);
 
 
         System.out.print("Enter account number: ");
-        user.setCount(sc.nextInt());
+        int tempNumber = sc.nextInt();
         sc.nextLine();
 
         System.out.print("Enter account holder: ");
-        user.setName(sc.nextLine());
+        String tempHolder = sc.nextLine();
+
 
         System.out.print("Is there na initial deposit (y/n)? ");
         char tempDeposit = sc.next().charAt(0);
         sc.nextLine();
+
         if (tempDeposit == 'y'){
-            System.out.print("Enter initial deposit value: ");
-            double amount = sc.nextDouble();
-            user.Deposit(tempDeposit,amount);
+            System.out.print("enter valuer: ");
+            double tempMoney = sc.nextDouble();
+            account = new Account(tempNumber, tempHolder, tempMoney);
+        }else{
+            account = new Account(tempNumber, tempHolder);
         }
 
         System.out.println("\n");
         System.out.println("Account data:");
-        System.out.print(user);
+        System.out.print(account);
 
 
         System.out.println("\n");
         System.out.print("Enter a deposit value: ");
-        double amount = sc.nextDouble();
-        user.Deposit(tempDeposit,amount);
+        account.deposit(sc.nextDouble());
 
         System.out.print("Updated account data");
-        System.out.print(user);
+        System.out.print(account);
         System.out.println("\n");
 
         System.out.print("Enter a withdraw value: ");
-        amount = sc.nextDouble();
-        user.removeAmount(amount);
+        account.removeAmount(sc.nextDouble());
         System.out.println("\n");
 
         System.out.print("Updated account data");
-        System.out.print(user);
+        System.out.print(account);
 
 
 
         /*debug*/
         System.out.println("\n****debug****\n");
-        System.out.println(user.getCount());
-        System.out.println(user.getName());
-        System.out.println(user.getMoney());
-
+        System.out.println("*  " + account.getCount());
+        System.out.println("*  "+account.getName());
+        System.out.println("*  "+account.getMoney());
+        System.out.println("\n****debug****\n");
 
         sc.close();
     }
--- java/ws-IntelliJ/execicioDeFixacao/bancario/main/src/entites/Account.java
+++ java/ws-IntelliJ/execicioDeFixacao/bancario/main/src/entites/Account.java
@@ -1,19 +1,27 @@
 package entites;
 
-public class User {
+public class Account {
 
     private int count;
     private String name;
     private Double money = 0.0;
 
 
+
+    public Account(int count, String name){
+        this.count = count;
+        this.name = name;
+    }
+    public Account(int count, String name, double money){
+        this.count = count;
+        this.name = name;
+        this.money = money;
+    }
+
     public int getCount() {
         return count;
     }
 
-    public void setCount(int count) {
-        this.count = count;
-    }
 
     public String getName() {
         return name;
@@ -30,18 +38,16 @@ public class User {
     public void setMoney(double money) {
         this.money = money;
     }
-    public void Deposit(int reply, double amount){
-        if(reply == 'y'){
-            setMoney(amount+getMoney());
-        }
-    }
 
+    public void deposit(double value){
+        money += value;
+    }
     public void removeAmount(double cashWithdrawal){
        setMoney(getMoney()-cashWithdrawal);
     }
 
 public String toString(){
-        return "Account " + getCount() + ", Holder:" + getName() + ", balance: R$ " + getMoney();
+        return "Account " + getCount() + ", Holder:" + getName() + ", balance: R$ "  + String.format("%.2f",getMoney());
 }
 
 }
